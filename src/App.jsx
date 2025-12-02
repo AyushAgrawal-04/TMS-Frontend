a187fae5176acfc8be3b508ac69cbf4b41467957
“AyushAgrawal-04” <ayushmagrawal.76@gmail.com>
2025-12-02 20:01:50 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">TMS</a>
+      </header>
+      <main>list product</main>
+    </div>
+  );
+}
