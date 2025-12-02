6ad2ef89d630b3b47d7cd9c148d80a030d06cb83
“AyushAgrawal-04” <ayushmagrawal.76@gmail.com>
2025-12-03 00:15:57 +0530

    connected backend server

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,30 +1,68 @@
-import { Link } from "react-router-dom";
-import data from "../data/data";
+import { Link } from 'react-router-dom';
+import { useEffect, useReducer, useState } from 'react';
+import axios from 'axios';
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'FETCH_REQUEST':
+      return { ...state, loading: true };
+    case 'FETCH_SUCCESS':
+      return { ...state, products: action.payload, loading: false };
+    case 'FETCH_FAIL':
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 
 function Homepage() {
-    return(
-  <>
-    <h1>Featured product</h1>
-    <div className="products">
-      {data.product.map((item) => (
-        <div className="product" key={item.slug}>
-          <Link to={`/product/${item.slug}`}>
-            <img src={item.image} alt={item.name} height={494} />
-          </Link>
+  const [{ loading, error, products }, dispatch] = useReducer(reducer, {
+    loading: true,
+    error: '',
+    products: [],
+  });
 
-          <div className="product-info">
-            <Link to={`/product/${item.slug}`}>
-              <p>{item.name}</p>
-            </Link>
-            <p>
-              <strong>${item.price}</strong>
-            </p>
-            <button>Add to cart</button>
-          </div>
-        </div>
-      ))}
-    </div>
-  </>);
+  useEffect(() => {
+    const fetchData = async () => {
+      dispatch({ type: 'FETCH_REQUEST' });
+      try {
+        const result = await axios.get('/api/products');
+        dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
+      } catch (err) {
+        dispatch({ type: 'FETCH_FAIL', payload: err.message });
+      }
+    };
+    fetchData();
+  }, []);
+  return (
+    <>
+      <h1>Featured product</h1>
+      <div className="products">
+        {loading ? (
+          <div>Loading...</div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
+          products.map((item) => (
+            <div className="product" key={item.slug}>
+              <Link to={`/product/${item.slug}`}>
+                <img src={item.image} alt={item.name} height={494} />
+              </Link>
+
+              <div className="product-info">
+                <Link to={`/product/${item.slug}`}>
+                  <p>{item.name}</p>
+                </Link>
+                <p>
+                  <strong>${item.price}</strong>
+                </p>
+                <button>Add to cart</button>
+              </div>
+            </div>
+          ))
+        )}
+      </div>
+    </>
+  );
 }
 export default Homepage;
